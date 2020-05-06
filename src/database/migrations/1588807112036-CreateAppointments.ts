cdeb0036bdb319b47bc7de061874a989e0ed4246
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-05-06 20:48:06 -0300

    Criando tabela de agendamentos

---
+++ src/database/migrations/1588807112036-CreateAppointments.ts
@@ -0,0 +1,46 @@
+// Criar migration
+// yarn typeorm migration:create -n CreateAppointments
+
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1588807112036
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+// Rodar migration
+// yarn typeorm migration:run
+
+// Reverter migration
+// yarn typeorm migration:revert
+
+// Mostrar migration criadas
+// yarn typeorm migration:show
